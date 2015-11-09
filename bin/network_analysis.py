1d22a2b0a769ce3dd2200e9572c73f794d5c84e9
Eric Leung <leunge@ohsu.edu>
2015-11-09 14:43:45 -0800

    add descriptions of each function as docstrings

--- bin/network_analysis.py
+++ bin/network_analysis.py
@@ -24,7 +24,6 @@ def get_prot_inter():
     print "There are %d edges in this network" % (len(ppList))
     return ppList
 
-# make larger graph
 def make_net(ppList):
     """
     DESCRIPTION: Make a graph object with adjancency list
@@ -36,20 +35,32 @@ def make_net(ppList):
         ppNet.add_edge(pp[0], pp[1]) # add interaction to network
     return ppNet
 
-# import TCGA nodes
 def get_TCGA():
+    """
+    DESCRIPTION: Import TCGA genes
+    INPUT: None
+    OUTPUT: List of imported TCGA genes
+    """
     tcga = []
     with open("../data/tcga_genes.txt", "r") as fh:
         for line in fh:
             tcga.append(line.rstrip())
     return tcga
 
-# make subgraph with TCGA genes
 def tcga_subgraph(ppNet, tcga):
+    """
+    DESCRIPTION: Make a subgraph with the TCGA genes
+    INPUT: Graph object and list of TCGA genes
+    OUTPUT: Subgraph (graph) object made with only TCGA genes
+    """
     return ppNet.subgraph(tcga)
 
-# find largest connected component of subgraph
 def largest_component(ppNet):
+    """
+    DESCRIPTION: Find largest connected component of subgraph
+    INPUT: Graph object
+    OUTPUT: Largest component subgraph
+    """
     subnetGraphs = nx.connected_component_subgraphs(ppNet)
     largest = nx.Graph() # empty graph
     greatN = 0
@@ -59,8 +70,13 @@ def largest_component(ppNet):
             greatN = len(sub.nodes())
     return largest
 
-# calculate centrality measures
 def calculate_center(tcgaSubgraph):
+    """
+    DESCRIPTION: Calculate centrality measures
+    INPUT: Graph object
+    OUTPUT: Dictionary of dictionaries, each being a different centrality
+    measure
+    """
     # calculate maximum eigenvalue of graph
     denseMat = nx.adjacency_matrix(tcgaSubgraph).todense() # convert to adj mat
     eigs = numpy.linalg.eig(denseMat)[0] # calculate eigenvalues
@@ -77,6 +93,12 @@ def calculate_center(tcgaSubgraph):
 
 # write file with centrality measures for nodes
 def write_central(centrality, centralName):
+    """
+    DESCRIPTION: Write a file with the centrality measures for nodes as a .tsv
+    file
+    INPUT: Dictionary of centrality and name of centrality
+    OUTPUT: A tab-delimited file of centrality measure
+    """
     fileName = "../data/" + centralName + "Rounded.tsv"
     sortedCenter = sorted(centrality.items(), key = operator.itemgetter(1),
             reverse = True)
@@ -84,13 +106,21 @@ def write_central(centrality, centralName):
         for pair in sortedCenter:
             fh.write(pair[0] + "\t" + str(round(pair[1], 4)) + "\n")
 
-# write file for each of the centralities
 def write_all(centers):
+    """
+    DESCRIPTION: Write file for each of the centralities
+    INTPUT: Dictionary with each of the centralities
+    OUTPUT: Separate file for each centrality measure
+    """
     for center in centers.keys():
         write_central(centers[center], center)
 
-# plot and save network
 def plot_net(graph, graphName):
+    """
+    DESCRIPTION: Plot and save network as image
+    INPUT: Graph object and desired name of image file
+    OUTPUT: Plotted network visualization as .png file 
+    """
     nx.draw(graph)
     saveName = "../images/" + graphName + ".png"
     plt.savefig(saveName)
@@ -118,8 +148,20 @@ def bfs_edges(subnet, source):
             queue.pop()
             queue.reverse()
 
-# main function
+def num_components(subgraph):
+    """
+    DESCRIPTION: Find number of components in graph
+    INPUT: Graph object
+    OUTPUT: Number of components
+    """
+    return 1
+
 def main():
+    """
+    DESCRIPTIOn: Main function to run entire analysis
+    INPUT: None
+    OUTPUT: The network analysis and plot of TCGA subgraph
+    """
     ppList = get_prot_inter()
     ppNet = make_net(ppList)
     tcga = get_TCGA()
