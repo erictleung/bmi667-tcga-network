aad55c1f06a7019dd753fb60a9ed6078cc328a15
Eric Leung <etleung21@gmail.com>
2015-11-08 19:16:34 -0800

    add bash and Python script for analysis

---
+++ bin/data.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Description: 
+#   Downloads the human protein-protein-interaction pairs in PSI-MITAB format
+#   and parses it for protein names in pairwise interactions.
+# Output:
+#   - homo_sapiens.mitab.interaction.txt (from Reactome website)
+#   - prot_interaction.tsv (pairwise prot-prot interactions)
+#   - all_unique.txt (list of unique genes in prot-prot interactions)
+# Dependencies:
+#   - curl (7.43.0 Tested)
+#   - gunzip (1.6 Tested)
+#   - awk (4.1.3 Tested)
+# Versions Tested: 
+#   bash 4.3.42
+#   bash 3.2.57
+
+# human protein-protein interaction pairs in PSI-MITAB format
+DIR=../data/
+FILE=homo_sapiens.mitab.interactions.txt
+ZIP="$FILE.gz"
+ADDRESS=http://www.reactome.org/download/current/$ZIP
+PAIR=prot_interaction.tsv
+FINAL=all_unique.txt
+
+# download file if file or zipped file not already downloaded
+if [ -f $DIR$ZIP ] || [ -f $DIR$FILE ]; then
+    echo "File already downloaded."
+else
+    curl $ADDRESS -o $DIR$ZIP
+fi
+echo "Successfully downloaded file."
+
+# unzip file
+if [ -f $DIR$FILE ]; then
+    echo "Found unzipped version of file."
+elif [ -f $DIR$ZIP ]; then
+    echo "Unzipping file previously downloaded."
+    gunzip $DIR$ZIP
+else
+    echo "Failed to download $FILE. Try again."
+    exit 1
+fi
+echo "Successfully unzipped file."
+
+# parse through file to get pairwise protein-protein interactions
+if [ -f $DIR$PAIR ]; then
+    echo "Pairwise interactions extracted already."
+else
+    echo "Extracting pairwise gene interactions."
+    awk -F '\t' '{ print $3 "\t" $4 }' $DIR$FILE | \
+        sed 1d | \
+        sed 's/.*:.*_\(.*\)(shortlabel)	.*:.*_\(.*\)(shortlabel)/\1	\2/' | \
+        awk '{ if ($1 != "-" && $2 != "-") print $1 "\t" $2 }' | \
+        awk '{ if ($1 > $2) print $2 "\t" $1; else print $1 "\t" $2 }' | \
+        sort -u > \
+        $DIR$PAIR
+fi
+echo "Successfully extracted pairwise gene interactions."
+
+# get unique list of genes list of interactions
+if [ -f $DIR$PAIR ]; then
+    cat $DIR$PAIR | \
+        awk '{ print $1 "\n" $2 }' | \
+        sort -u > \
+        $DIR$FINAL
+fi
---
+++ bin/network_analysis.py
@@ -0,0 +1,72 @@
+#!/bin/pyhon
+
+# import packages
+import networkx as nx
+import operator
+import matplotlib.pyplot as plt
+
+# import interactions as edge list
+def get_prot_inter():
+    ppList = []
+    with open("prot_interaction.tsv", "r") as fh:
+        for line in fh:
+            splitLine = line.rstrip().split("\t")
+            ppList.append(splitLine)
+    return ppList
+
+# make larger graph
+def make_net(ppList):
+    ppNet = nx.Graph() # create empty network
+    for pp in ppList:
+        ppNet.add_edge(pp[0], pp[1]) # add interaction to network
+    return ppNet
+
+# import TCGA nodes
+def get_TCGA():
+    tcga = []
+    with open("tcga_genes.txt", "r") as fh:
+        for line in fh:
+            tcga.append(line.rstrip())
+    return tcga
+
+# make subgraph with TCGA genes
+def tcga_subgraph(ppNet, tcga):
+    return ppNet.subgraph(tcga)
+
+# calculate centrality measures
+def calculate_center(tcgaSubgraph):
+    centers = {}
+    centers["eigen"] = nx.eigenvector_centrality(tcgaSubgraph)
+    centers["degree"] = nx.degree_centrality(tcgaSubgraph)
+    centers["katz"] = nx.katz_centrality_numpy(tcgaSubgraph)
+    centers["pagerank"] = nx.pagerank(tcgaSubgraph)
+    return centers
+
+# write file with centrality measures for nodes
+def write_central(centrality, centralName):
+    fileName = centralName + "Rounded.tsv"
+    sortedCenter = sorted(centrality.items(), key = operator.itemgetter(1),
+            reverse = True)
+    with open(fileName, "w") as fh:
+        for pair in sortedCenter:
+            fh.write(pair[0] + "\t" + str(round(pair[1], 4)) + "\n")
+
+# write file for each of the centralities
+def write_all(centers):
+    for center in centers.keys():
+        write_central(centers[center], center)
+
+# plot and save network
+def plot_net(graph, graphName):
+    nx.draw(graph)
+    saveName = graphName + ".png"
+    plt.savefig(saveName)
+
+if __name__ == '__main__':
+    ppList = get_prot_inter()
+    ppNet = make_net(ppList)
+    tcga = get_TCGA()
+    tcgaSubgraph = tcga_subgraph(ppNet, tcga)
+    centers = calculate_center(tcgaSubgraph)
+    write_all(centers)
+    plot_net(tcgaSubgraph, "tcga_subgraph")
